d5b945c643b352d24f799e79444b922599a041fc
BenitoSwaggolini <profie_player@mail.ru>
2023-08-26 23:00:01 +0500

    base files in modules

---
+++ core/app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from app.telegram.app import app as telegram_app
+from app.auth.app import app as auth_app
+app = FastAPI()
+
+#include routers
+app.include_router(telegram_app, prefix="api")
+
+app.include_router(auth_app)
+
+#add protection
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
---
+++ core/settings.py
@@ -0,0 +1,19 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    TELEGRAM_TOKEN: str
+    DB_CONNECTION: str
+    DB_MIGRATE_PATH: str
+    WEBHOOK_URL: str
+    REDIS_CONNECTION: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    SECRET_KEY: str
+    ALGORITM: str
+
+    class Config:
+        env_file = "env/.env"
+        encoding = "utf-8"
+
+
+settings = Settings()
